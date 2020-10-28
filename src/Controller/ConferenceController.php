d5d354c1822888f04c3cc7f89d4ec91fa509e715
Iulian Beleiu <iulian.beleiu@gmail.com>
2020-10-28 21:26:42 +0200

    display form in template

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,6 +30,9 @@ class ConferenceController extends AbstractController
         Conference $conference,
         CommentRepository $commentRepository
     ) {
+        $commet = new Comment();
+        $form = $this->createForm(CommentFormType::class, $commet);
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -36,6 +41,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]);
     }
 }
